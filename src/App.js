5b529598cbe00c70d49923fde85d41cb8fdb40ba
Rana <ranab33@gmail.com>
2020-08-06 18:30:03 -0400

    working with URL params

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Food from  "./Food";
+import { Route } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Food name='Kale' />
+        <Route exact path ="/food/:name"  render={routeProps => <Food {...routeProps} />} />
       </div>
     );
   }
