2822eaee7b527f42ce19a23dae609f25359be84b
Rana <ranab33@gmail.com>
2020-08-06 20:32:57 -0400

    add simple search form, add redirect component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import Food from  "./Food";
-import { Route } from "react-router-dom";
+import Meal from  "./Meal";
+import FoodSearch from "./FoodSearch";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Switch>
         <Route exact path ="/food/:name"  render={routeProps => <Food {...routeProps} />} />
+        <Route exact path="/food/:foodName/drink/:drinkName" component={Meal}/>
+        <Route exact path="/" render={() => <FoodSearch />} />
+        <Route render={() => <h1>ERROR! NOT FOUND!</h1>}/>
+        </Switch>
       </div>
+
     );
   }
 }
