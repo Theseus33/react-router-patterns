4d6e08ea4a593a90158daac54eadfb636ff47b34
Rana <ranab33@gmail.com>
2020-08-06 21:17:29 -0400

    add pushing onto the history prop. add withRouter component, add back button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import Food from  "./Food";
 import Meal from  "./Meal";
+import Navbar from "./Navbar";
 import FoodSearch from "./FoodSearch";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import { findByDisplayValue } from '@testing-library/react';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar/>
         <Switch>
-        <Route exact path ="/food/:name"  render={routeProps => <Food {...routeProps} />} />
+        <Route exact path ="/food/:name"  
+        render={routeProps => 
+        <Food {...routeProps} />} />
         <Route exact path="/food/:foodName/drink/:drinkName" component={Meal}/>
-        <Route exact path="/" render={() => <FoodSearch />} />
+        <Route exact path="/" component={FoodSearch}/>
         <Route render={() => <h1>ERROR! NOT FOUND!</h1>}/>
         </Switch>
       </div>
--- src/FoodSearch.js
+++ src/FoodSearch.js
@@ -6,9 +6,16 @@ class FoodSearch extends Component {
         super(props);
         this.state = {query: ""};
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
     handleChange(evt){
         this.setState({query: evt.target.value });
+    }
+    handleClick(){
+        //Do something
+        alert("Saved your food to DB!");
+        //redirect to somewhere else
+        this.props.history.push(`/food/${this.state.query}`);
     }
     render() {
         return (
@@ -19,6 +26,7 @@ class FoodSearch extends Component {
                 value={this.state.query} 
                 onChange={this.handleChange}/>
                 <Link to={`/food/${this.state.query}`}>Go!</Link>
+                <button onClick={this.handleClick}> Save New Food!</button>
             </div>
         );
     }
